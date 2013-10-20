5a2b5f2da4100cbac6407fd04562b4f1d89095ab
Pavan Gupta <pavan.gupta@synerzip.com>
2013-10-20 20:08:09 +0530

    Added TestNG.xml and testng target in build.xml

--- src/sample/TestingSelenium.java
+++ src/sample/TestingSelenium.java
@@ -1,31 +1,25 @@
 package sample;
 
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestingSelenium {
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
-    System.out.println("Hello");
-    
+ 
+  @Test
+  public void gmail() {
+    // TODO Auto-generated method stub    
     WebDriver driver = new FirefoxDriver();
     driver.get("https://www.gmail.com");
-    
-    
     String title = driver.findElement(By.xpath("html/body/div[1]/div[2]/div[2]/div[1]/h1")).getText();
     if(title.equals("Gmail"))
        System.out.println("Hurray! its GMail");
     else
       System.out.println("Oops! we are at wrong place");
-   
-    driver.quit();
-   
   
+    driver.quit(); 
   }
 
 }
