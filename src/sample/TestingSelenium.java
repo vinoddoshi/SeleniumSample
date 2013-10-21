828835a04ff53d44bc618009d987582730ec09f5
vinod doshi <vinod_doshi@mail.example.com>
2013-10-21 13:58:41 +0530

    Making a test fail

--- src/sample/TestingSelenium.java
+++ src/sample/TestingSelenium.java
@@ -15,7 +15,7 @@ public class TestingSelenium {
     WebDriver driver = new FirefoxDriver();
     driver.get("https://www.gmail.com");
     String title = driver.findElement(By.xpath("html/body/div[1]/div[2]/div[2]/div[1]/h1")).getText();
-    Assert.assertTrue(title.equals("Gmail"),"Hurray! its GMail");
+    Assert.assertTrue(title.equals("Gmaill"),"Hurray! its GMail");
        
      // System.out.println("Oops! we are at wrong place");
   
